0aa034be4d6285ee5d3c0acc52677db8e32de1f4
Anoop Rajendra <anoop.rajendra@gmail.com>
2011-10-07 22:52:40 +0000

    Bug fix. Change permissions of only the recipient filesystem

--- src/zfs_backup/zfs-perms-setup.sh
+++ src/zfs_backup/zfs-perms-setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# $Id: zfs-perms-setup.sh,v 1.2 2011/10/07 22:52:40 anoop Exp $
+
+# $Log: zfs-perms-setup.sh,v $
+# Revision 1.2  2011/10/07 22:52:40  anoop
+# Bug fix. Change permissions of only the recipient filesystem
+#
+
 # This script configures zfs permissions for the 
 # replication user created as part of the ROCKS 
-# thumper-conf roll
+# ZFS-storage roll
 
 # Usage
 if [ "$1" == "" ]; then
@@ -15,11 +22,10 @@ ZFS_USER="zfsuser"
 ZFS_PERM_SET="@replication"
 ZFS_FILESYSTEM=$1
 
-ZFS_MOUNT=`zfs get -H -o value mountpoint`
+ZFS_MOUNT=`zfs get -H -o value mountpoint $ZFS_FILESYSTEM`
 
 # Set permissions
 /bin/chmod A+user:$ZFS_USER:add_subdirectory:fd:allow $ZFS_MOUNT
 /sbin/zfs allow -s $ZFS_PERM_SET \
 	create,destroy,mount,receive,send,snapshot $ZFS_FILESYSTEM
 /sbin/zfs allow $ZFS_USER $ZFS_PERM_SET $ZFS_FILESYSTEM
-
